957e78ae8c083e6c7a82df28a2bfb45e739ae7f3
jorgerincon83 <tecrinconj@gmail.com>
2021-11-22 20:27:56 -0500

    codigo terminado

---
+++ javascript_introduccion/Funciones.js
@@ -0,0 +1,59 @@
+/*function mensaje(){
+    return 'Mensaje desde la funcion'; 
+}
+var msj = mensaje();
+console.log(msj);*/
+
+/*function sumar (a,b,c){
+    return a+b+c;
+}
+var result = sumar(4, 5, 6);
+var result1 = sumar(3, 7);
+console.log(result);
+console.log(result1);*/
+
+/*** funciones recursivas  */
+
+/*var factorial = function(n){
+    if((n == 0) || (n == 1))
+        return 1;
+    else
+        return ( n * factorial(n - 1));
+}
+console.log(factorial(5));*/
+
+/**** funciones predefinidas */
+
+/*var nombres = ['jorge','carlos','ana','tatiana'];
+var vegetales = new Array('tomate','lechuga','zanahoria');
+console.log(nombres (3));
+console.log(vegetales(1));
+nombres [0] = 'jose';
+vegetales [2] = 'lechiga'
+console.log(nombres [0]);
+console.log(vegetales [2]);
+
+console.log(nombres.length);
+for(var i = 0; i<= nombres.length -1; i++){
+    console.log(nombres(i));
+}
+vegetales.forEach(function(elemnto, indice){
+console.log(elemnto,indice);
+})
+
+console.log(nombres);
+nombres.push('pedro'); /*para agregar mas indices*/
+/*console.log(nombres);
+nombres.unshift('lili'); /*para agregar mas indices*/
+/*console.log(nombres);
+nombres.pop();
+console.log(nombres);
+nombres.shift();
+console.log(nombres);
+
+var pos = nombres.indexOf('carlos');
+console.log(pos);
+nombres.splice(1, 2);
+console.log(nombres);*/
+
+
--- javascript_introduccion/app.js
+++ javascript_introduccion/app.js
@@ -37,7 +37,7 @@ console.log(yearActual / 2);*/
 /* Operadores Logicos unarios y de asignacion
 logicos < > <= >= ==
 */
-var edadAna, edadMaria, edadTatiana;
+/*var edadAna, edadMaria, edadTatiana;
 edadAna = 34;
 edadMaria = 28;
  var mayorAna = edadAna > edadMaria;
@@ -46,14 +46,40 @@ edadMaria = 28;
  ejemplo edadAna++; 
  
  */
-console.log(edadAna++);
+/*console.log(edadAna++);
 console.log(edadAna);
 /* Operadores de asignacion
 += -= *= /= %= */
-var a = 11;
+/*var a = 11;
 var b = 5;
 var c = a % 5; // residuo o resto de la division
-console.log(c);
+console.log(c);*/
+
+/*var nombre = 'Jorge';
+var esCasado = true;
+if(esCasado == true){
+    console.log(nombre + ' Es Casado.');
+}else {
+    console.log(nombre + 'Es Soltero');
+}*/
+
+var nombre = 'jorge';
+var edad = 8
+
+if(edad < 12){
+    console.log(nombre + 'es un niño.');
+}else if ((edad > 11 && edad < 18)){
+    console.log(nombre + ' es un adolecente');
+}else if((edad > 17) && (edad < 60)){
+    console.log(nombre + 'es un adulto.');
+
+}else{
+    console.log(nombre + 'es un anciano.');
+}
+
+
+
+
 
 
 
